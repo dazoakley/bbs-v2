6e946d99f85fca1fb219da177820975b640ee587
Darren Oakley <daz.oakley@gmail.com>
2013-05-29 11:36:42 +0100

    Update the second chart::clicker example to make a bar chart.

--- chapter-6/section-6.5/05-chart-clicker2.pl
+++ chapter-6/section-6.5/05-chart-clicker2.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 use Chart::Clicker;
-use Chart::Clicker::Renderer::Area;
+use Chart::Clicker::Renderer::Bar;
 
 # Create a Chart::Clicker object.
 my $cc = Chart::Clicker->new(
@@ -13,22 +13,32 @@ my $cc = Chart::Clicker->new(
   format => 'png'
 );
 
-# Add data to our chart.
-$cc->add_data('Set 1', [5.8, 5.0, 4.9, 4.8, 4.5, 4.2]);
-$cc->add_data('Set 2', [0.7, 1.1, 1.7, 2.5, 3.0, 4.5]);
+# Add data to our chart. We use a hash this time to represent the
+# data as we're drawing a bar chart and we want to say wich group
+# the values belong to.
+$cc->add_data('Set 1', { 1 => 5.8, 2 => 5.0, 3 => 4.9, 4 => 4.8 });
+$cc->add_data('Set 2', { 1 => 0.7, 2 => 1.1, 3 => 1.7, 4 => 2.5 });
 
 # Set a title for the chart.
-$cc->title->text('Area Chart');
+$cc->title->text('Bar Chart');
 $cc->title->padding->bottom(5);
 
 # Replace the standard (Line) renderer.
-my $renderer = Chart::Clicker::Renderer::Area->new(
-  opacity => 0.75
-);
+my $renderer = Chart::Clicker::Renderer::Bar->new(opacity => .6);
 $cc->set_renderer($renderer);
 
+# Get the image 'context' and set some values
+my $def = $cc->get_context('default');
+$def->range_axis->baseline(0); # Make the y-axis start at zero
+$def->domain_axis->tick_values([qw(1 2 3 4)]); # x-axis labels
+$def->domain_axis->format('%d'); # x-axis label formatting
+
+# We ask clicker to "fudge" the edges with some padding so that the bars show
+# up properly – this is bug that we unfortunately have to work around.
+$def->domain_axis->fudge_amount(.25);
+
 # Finally, save our image.
-$cc->write_output('chart_clicker_area.png');
+$cc->write_output('chart_clicker_bar.png');
 
 exit;
 
